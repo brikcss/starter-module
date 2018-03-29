b856de558e9bac24fecea8b1131835cd8038a4a7
Zimmee <thezimmee@gmail.com>
2018-03-29 16:55:26 -0600

    feature(tests): Add boilerplate code to test placeholder.
    [BRANCH: master]

--- test/lib.spec.js
+++ test/lib.spec.js
@@ -1,6 +1,11 @@
 /* eslint-env mocha */
 // const assert = require('assert');
+const rm = require('rimraf');
 
 describe('lib()', () => {
+	afterEach(() => {
+		rm.sync('.temp');
+	});
+
 	it('should do something amazing');
 });
