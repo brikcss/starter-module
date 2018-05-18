13364c918919e16933c9b5ad03cf93c228a46965
Zimmee <thezimmee@gmail.com>
2018-05-18 16:37:39 -0600

    fix(postcss): Audit `postcss config` and organize.
    [BRANCH: master]

--- .postcssrc.js
+++ .postcssrc.js
@@ -18,6 +18,11 @@ module.exports = {
 			// addModulesDirectories: []
 		}),
 
+		// Add custom css/js mixins.
+		require('postcss-mixins')({
+			mixinsDir: './src/mixins'
+		}),
+
 		// Manages assets.
 		require('postcss-assets')({
 			basePath: 'src/',
@@ -31,82 +36,6 @@ module.exports = {
 		// Rebase, inline, or copy assets. Works with postcss-assets.
 		require('postcss-url')(),
 
-		// Add custom css/js mixins.
-		require('postcss-mixins')({
-			mixinsDir: './src/mixins'
-		}),
-
-		// Polyfill future css features based on supported browsers.
-		require('postcss-preset-env')({
-			// Determines which css features to polyfill.
-			stage: 1,
-			// Toggle features.
-			features: [],
-			// Determine which features to polyfill based on browserslist.
-			browsers: 'last 2 versions'
-		}),
-
-		// Handled by postcss-preset-env. Polyfills css variables/properties.
-		// require('postcss-css-variables')({
-		// 	preserve: true
-		// }),
-
-		// Enables custom property sets.
-		require('postcss-apply')({
-			preserve: true
-		}),
-
-		// Handled by postcss-preset-env. Polyfills image set function.
-		// require('postcss-image-set-polyfill')(),
-
-		// Enables nested ancestor selectors.
-		require('postcss-nested-ancestors')({
-			placeholder: '^&'
-		}),
-
-		// Handled by postcss-preset-env. Polyfills nested selectors.
-		// require('postcss-nested')(),
-
-		// Handled by postcss-preset-env. Polyfills custom media variables.
-		// require('postcss-custom-media')({
-		// 	extensions: {
-		// 		'--phone': '(min-width: 480px)',
-		// 		'--tablet': '(min-width: 768px)',
-		// 		'--desktop': '(min-width: 990px)',
-		// 		'--wide': '(min-width: 1400px)'
-		// 	},
-		// 	preserve: true,
-		// 	appendExtensions: false
-		// }),
-
-		// Packs multiple media queries into one.
-		require('css-mqpacker')(),
-
-		// Handled by postcss-preset-env. Polyfills custom selector variables.
-		// require('postcss-custom-selectors')({ extensions: {}, transformMatches: false }),
-
-		// Handled by postcss-preset-env. Polyfills hwb() color function.
-		// require('postcss-color-hwb')(),
-
-		// Handled by postcss-preset-env. Polyfills hsl() color function.
-		// require('postcss-color-hsl')(),
-
-		// Handled by postcss-preset-env. Polyfills gray() color function.
-		// require('postcss-color-gray')(),
-
-		// Handled by postcss-preset-env. Polyfills color-mod() function.
-		require('postcss-color-mod-function')(),
-
-		// Combines colors within a specificed threshold to reduce number of color values.
-		// require('colorguard')({
-		// 	ignore: [],
-		// 	threshold: 3,
-		// 	whitelist: [[]]
-		// }),
-
-		// Automatically apply system ui font family.
-		// require('postcss-font-family-system-ui')()
-
 		// Auto generate @font-face rules.
 		// require('postcss-font-magician')({
 		// 	variants: {}, // Download specific variants.
@@ -117,8 +46,83 @@ module.exports = {
 		// 	custom: {}, // Custom settings.
 		// }),
 
-		// Handled by postcss-preset-env. Polyfills `initial` keyword.
-		// require('postcss-initial')(),
+		// Polyfills css variables/properties.
+		require('postcss-css-variables')({
+			preserve: true
+		}),
+
+		// Enables custom property sets.
+		require('postcss-apply')({
+			preserve: true
+		}),
+
+		// Polyfill future css features based on supported browsers.
+		require('postcss-preset-env')({
+			// Determines which css features to polyfill.
+			stage: 1,
+			// Toggle features.
+			features: {
+				'custom-properties': false,
+				'custom-property-sets': false,
+				'image-set-function': {},
+				'logical-properties-and-values': false,
+				'nesting-rules': {},
+				'custom-media-queries': {
+					extensions: {
+						'--phone': '(min-width: 480px)',
+						'--tablet': '(min-width: 768px)',
+						'--desktop': '(min-width: 990px)',
+						'--wide': '(min-width: 1400px)'
+					},
+					preserve: true,
+					appendExtensions: false
+				},
+				'media-query-ranges': {},
+				'custom-selectors': { extensions: {}, transformMatches: false },
+				'case-insensitive-attributes': {},
+				'rebeccapurple-color': false,
+				'hexadecimal-alpha-notation': false,
+				'lab-function': false,
+				'color-mod-function': {},
+				'color-functional-notation': {},
+				'system-ui-font-family': {},
+				'font-variant-property': {},
+				'all-property': {},
+				'matches-pseudo-class': {},
+				'not-pseudo-class': {},
+				'any-link-pseudo-class': {},
+				'dir-pseudo-class': false,
+				'break-properties': {},
+				'gap-properties': {},
+				'overflow-property': {},
+				'overflow-wrap-property': {},
+				'place-properties': {},
+				'focus-visible-pseudo-class': {},
+				'focus-within-pseudo-class': {}
+			},
+			// Determine which features to polyfill based on browserslist.
+			browsers: 'last 2 versions',
+			// Insert plugins into the chain.
+			insertBefore: {
+				'nesting-rules': [
+					// Enables nested ancestor selectors.
+					require('postcss-nested-ancestors')({
+						placeholder: '^&'
+					})
+				]
+			},
+			insertAfter: {}
+		}),
+
+		// Packs multiple media queries into one.
+		require('css-mqpacker')(),
+
+		// Combines colors within a specificed threshold to reduce number of color values.
+		// require('colorguard')({
+		// 	ignore: [],
+		// 	threshold: 3,
+		// 	whitelist: [[]]
+		// }),
 
 		// Convert pixel values to rems.
 		require('postcss-pxtorem')({
@@ -130,15 +134,6 @@ module.exports = {
 			mediaQuery: false,
 			minPixelValue: 0
 		})
-
-		// Handled by postcss-preset-env. Polyfills :matches() selector.
-		// require('postcss-selector-matches')(),
-
-		// Handled by postcss-preset-env. Polyfills :not() selector with multiple values.
-		// require('postcss-selector-not')(),
-
-		// Handled by postcss-preset-env. Polyfills :any-link selector.
-		// require('postcss-pseudo-class-any-link')(),
 
 		// Adds specified prefix to all classes and ids.
 		// require('postcss-prefixer')({
