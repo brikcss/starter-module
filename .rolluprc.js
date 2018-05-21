6bc8bc8a048ab45aec94acbe90ceb51e4592d158
Zimmee <thezimmee@gmail.com>
2018-05-21 08:13:14 -0600

    chore(package.json): Update `dependencies` in package.json.
    [BRANCH: master]

--- .rolluprc.js
+++ .rolluprc.js
@@ -12,7 +12,7 @@ import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import prettier from 'rollup-plugin-prettier';
 const prettierConfig = require('./.prettierrc.js');
-import uglify from 'rollup-plugin-uglify';
+import uglify from 'rollup-plugin-terser';
 import pkg from './package.json';
 
 // Flags.
