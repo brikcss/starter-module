2e35790c5d48f291e3434557aa642832496841e9
Zimmee <thezimmee@gmail.com>
2018-10-22 16:58:57 -0600

    feature(prettier): Update prettier config.
    [BRANCH: master]

--- .prettierrc.js
+++ .prettierrc.js
@@ -5,51 +5,14 @@
  ** --------------------------------------------------------------------------------------------- */
 
 module.exports = {
-	printWidth: 100,
-	singleQuote: true,
-	useTabs: true,
-	tabWidth: 4,
-	semi: true,
-	arrowParens: 'always'
-	// parser: 'babylon'
-	// overrides: [
-	// 	{
-	// 		files: '.prettierrc',
-	// 		options: {
-	// 			parser: 'json'
-	// 		}
-	// 	},
-	// 	{
-	// 		files: '.*rc',
-	// 		options: {
-	// 			parser: 'json'
-	// 		}
-	// 	},
-	// 	{
-	// 		files: '*.js',
-	// 		options: {
-	// 			parser: 'babel'
-	// 		}
-	// 	},
-	// 	{
-	// 		files: '.json',
-	// 		options: {
-	// 			parser: 'json'
-	// 		}
-	// 	},
-	// 	{
-	// 		files: '*.{yaml,yml}',
-	// 		options: {
-	// 			useTabs: false,
-	// 			tabWidth: 2
-	// 		}
-	// 	},
-	// 	{
-	// 		files: '*.md',
-	// 		options: {
-	// 			parser: 'markdown',
-	// 			printWidth: false
-	// 		}
-	// 	},
-	// ]
+  arrowParens: 'always',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  printWidth: 100,
+  semi: true,
+  singleQuote: true,
+  tabs: false,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
 };
