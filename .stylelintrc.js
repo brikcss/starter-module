03bf99a13e0bd8d37f5b8b156546635fc44e35fe
Zimmee <thezimmee@gmail.com>
2019-05-23 04:41:23 -0600

    fix(stylelint): remove stylelint config file (already included in @brikcss/git-hooks).
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    #
    # On branch master
    # Your branch is ahead of 'origin/master' by 2 commits.
    #   (use "git push" to publish your local commits)
    #
    # Changes to be committed:
    #	deleted:    .stylelintrc.js
    #
    
    [BRANCH: master]

--- .stylelintrc.js
+++
@@ -1,5 +0,0 @@
-/*! .stylelintrc.js | @author Brikcss (https://github.com/brikcss) | @reference (https://stylelint.io/user-guide/configuration) */
-
-module.exports = {
-  extends: '@brikcss/stylelint-config-css'
-}
